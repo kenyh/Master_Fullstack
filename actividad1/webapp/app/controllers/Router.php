dc2c2c147025ac51c8295280356a004df8b465eb
Jorge Melnik <jmelnik19@gmail.com>
2026-01-02 10:28:16 -0300

    simplificando el router

--- actividad1/webapp/app/controllers/Router.php
+++ actividad1/webapp/app/controllers/Router.php
@@ -1,6 +1,7 @@
 <?php
 class Router
 {
+    //controllerName lo hacemos static para que sea facil detectar cual es el controlador usado en la vista y pintar la opción correcta en el menú.
     static public $controllerName = "HomeController"; //Si no hay nada despues de la primer / es el Home
     protected $controller = null;
     protected $method = 'list'; //Por defecto llamamos al método list del controlador
@@ -8,33 +9,30 @@ class Router
 
     public function __construct()
     {
-        $url = $this->parseUrl();
+        $urlArray = $this->urlToArray();
 
-        if (isset($url[0])) {               //Si existe algo despues del primer /, cambio el controller name
-            Router::$controllerName = ucfirst($url[0]) . 'Controller';
+        if (isset($urlArray[0])) {          //Si hay elementos en urlArray. O sea, si la ruta no es / a secas.
+            Router::$controllerName = ucfirst($urlArray[0]) . 'Controller';
+        }
+        if (isset($urlArray[1])) {          //Si existe un segundo elemento en urlArray.
+            $this->method = $urlArray[1];
         }
 
         //instancio el controlador en base al controllerName, home o el seteado en el if anterior.
         require_once Router::$controllerName . '.php';
         $this->controller = new Router::$controllerName;
 
-
-        if (isset($url[1])) {   //Si existe un segundo barra algo.
-            $this->method = $url[1];
-        }
-
-        if (!method_exists($this->controller, $this->method)) { //Si no existe el método en el controlador
+        if (!method_exists($this->controller, $this->method)) {                     //Si no existe el método en el controlador
             throw new Exception("No existe el método " . $this->method . " en el controlador " . Router::$controllerName);
         }
 
         call_user_func_array([$this->controller, $this->method], $this->params);    //Invocamos el método del controlador de esta forma rara. 
     }
 
-    private function parseUrl()
-    {
-        //ej de $_GET['url'] : platforms/create
-        if (!isset($_GET['url'])) { // Si no hay url retorno array vacío.
-            return ["home"];
+    private function urlToArray()
+    {                                       //ej de $_GET['url'] : platforms/create
+        if (!isset($_GET['url'])) {
+            return [];                      // Si no hay url retorno array vacío.
         }
         $url = rtrim($_GET['url'], '/');    //Sin espacios y sin la barra final.
         return explode('/', $url);          //Convierto los valores separados por / de la url en un array.
