bcbbb63305eef0b32325d6993673626cb989b2c0
Jorge Melnik <jmelnik19@gmail.com>
2026-01-10 12:06:11 -0300

    Squashed commit of the following:
    commit 52dd0c0899e2e1037f0062b1b8b6f4c31f8b1e36
    Author: Jorge Melnik <jmelnik19@gmail.com>
    Date:   Sat Jan 10 12:04:24 2026 -0300
    
        pronta?
    
    commit 26ea056daf051bf788c9f10199aed7bd9bb440be
    Author: Jorge Melnik <jmelnik19@gmail.com>
    Date:   Fri Jan 9 20:56:05 2026 -0300
    
        mejoras en marcadores
    
    commit 945d9505c611c37f01172b4ac99e0f0f948f85ec
    Author: Jorge Melnik <jmelnik19@gmail.com>
    Date:   Fri Jan 9 20:40:40 2026 -0300
    
        sin objeto COLOR
    
    commit 31a3729279bf8446fc963cf6012e5d66701fa2a5
    Author: Jorge Melnik <jmelnik19@gmail.com>
    Date:   Fri Jan 9 20:33:15 2026 -0300
    
        comentarios
    
    commit 839fb130d07f524bbdc8f5aae22c56411143524a
    Author: Jorge Melnik <jmelnik19@gmail.com>
    Date:   Fri Jan 9 20:25:26 2026 -0300
    
        mazoAceptaCarta anda
    
    commit 0d4fd5bf13e027ba6243ec755640d3e5e601eef4
    Author: Jorge Melnik <jmelnik19@gmail.com>
    Date:   Fri Jan 9 19:36:58 2026 -0300
    
        PALO_COLOR
    
    commit 93b9173b855f3fe0d32dfd5c56ea8596d79c1ebc
    Author: Jorge Melnik <jmelnik19@gmail.com>
    Date:   Thu Jan 8 20:36:05 2026 -0300
    
        simplificado para almacenar SOLO en el html si es posible
    
    commit a173784aff13b9a78870daddd26dd11992f05848
    Author: Jorge Melnik <jmelnik19@gmail.com>
    Date:   Thu Jan 8 20:06:33 2026 -0300
    
        sin mazoInicial global
    
    commit 9030537486474d62e538573e2fc7225554087d3e
    Author: Jorge Melnik <jmelnik19@gmail.com>
    Date:   Thu Jan 8 19:52:58 2026 -0300
    
        simplificando el código.
    
    commit 3981fef5df1d6fce0795e5b89a329e2e0eef9f23
    Author: Jorge Melnik <jmelnik19@gmail.com>
    Date:   Thu Jan 8 19:17:18 2026 -0300
    
        logica draggable true o false
    
    commit 7a4a2acf6a3a2c1be0dd84351cebc51bde6a2119
    Author: Jorge Melnik <jmelnik19@gmail.com>
    Date:   Thu Jan 8 19:01:10 2026 -0300
    
        orden
    
    commit 32f025064783901c480239d47eb242c5436c0e26
    Author: Jorge Melnik <jmelnik19@gmail.com>
    Date:   Thu Jan 8 18:58:55 2026 -0300
    
        sacamos ondrop de mazo_inicial
    
    commit af15a23e3d788a4026ed21d22d346a506fa025ff
    Author: Jorge Melnik <jmelnik19@gmail.com>
    Date:   Thu Jan 8 18:48:10 2026 -0300
    
        palo y numero en datatransfer
    
    commit 08a78dff4e3ef48ab2069079673e1f968fb86d40
    Author: Jorge Melnik <jmelnik19@gmail.com>
    Date:   Wed Jan 7 20:58:15 2026 -0300
    
        cambiamos nombre cartas por mazo.
    
    commit 51f0ca9787051de129307dbda7958b37c40c3da0
    Author: Jorge Melnik <jmelnik19@gmail.com>
    Date:   Wed Jan 7 20:42:05 2026 -0300
    
        contadores suman y restan
    
    commit 2de1f465860ec9853f3201ec9f764196005dd0d9
    Author: Jorge Melnik <jmelnik19@gmail.com>
    Date:   Wed Jan 7 16:32:07 2026 -0300
    
        detallito para cuando no hay nada en el mazo
    
    commit 3f70a749e6ff38e17bf3119ee12aaef4ecdb8832
    Author: Jorge Melnik <jmelnik19@gmail.com>
    Date:   Wed Jan 7 16:14:48 2026 -0300
    
        primer drag andando
    
    commit 483adba728f42f89cdf778b308c10d249fdeb0ea
    Author: Jorge Melnik <jmelnik19@gmail.com>
    Date:   Wed Jan 7 15:25:10 2026 -0300
    
        vuelta a estilos del profe
    
    commit d56539b46436a51ca936b6296d6a0ecfb85369f2
    Author: Jorge Melnik <jmelnik19@gmail.com>
    Date:   Wed Jan 7 10:00:57 2026 -0300
    
        puesta a cero de contadores de maso donde se indicaba
    
    commit 2de8f99f85dc5501445beaaa578ae654d578bf4e
    Author: Jorge Melnik <jmelnik19@gmail.com>
    Date:   Wed Jan 7 08:56:18 2026 -0300
    
        contadores cargados
    
    commit 1aa19eee4d719c6a20035ed1691e2b837cd085b2
    Author: Jorge Melnik <jmelnik19@gmail.com>
    Date:   Tue Jan 6 20:38:42 2026 -0300
    
        primer commit con cambios

--- frontend/actividad1/html/js/solitario.js
+++ frontend/actividad1/html/js/solitario.js
@@ -1,168 +1,184 @@
 /***** INICIO DECLARACIÓN DE VARIABLES GLOBALES *****/
 
-// Array de palos
-let palos = ["viu", "cua", "hex", "cir"];
-// Array de número de cartas
-//let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-// En las pruebas iniciales solo se trabajará con cuatro cartas por palo:
-let numeros = [9, 10, 11, 12];
+const mazoInicial = document.getElementById("mazo-inicial"); //Mazo inicial se usa en varios lados.
+const mazoSobrantes = document.getElementById("mazo-sobrantes"); //Mazo sobrantes se usa en varios lados.
+const contMovimientos = document.getElementById("contador_movimientos"); //Acá porque se usa en 2 lugares.
+let temporizador = null; // Lo dejamo global para poderle hacer el clear.
 
-// paso (top y left) en pixeles de una carta a la siguiente en un mazo
-let paso = 5;
-
-// Tapetes				
-let tapeteInicial   = document.getElementById("inicial");
-let tapeteSobrantes = document.getElementById("sobrantes");
-let tapeteReceptor1 = document.getElementById("receptor1");
-let tapeteReceptor2 = document.getElementById("receptor2");
-let tapeteReceptor3 = document.getElementById("receptor3");
-let tapeteReceptor4 = document.getElementById("receptor4");
-
-// Mazos
-let mazoInicial   = [];
-let mazoSobrantes = [];
-let mazoReceptor1 = [];
-let mazoReceptor2 = [];
-let mazoReceptor3 = [];
-let mazoReceptor4 = [];
-
-// Contadores de cartas
-let contInicial     = document.getElementById("contador_inicial");
-let contSobrantes   = document.getElementById("contador_sobrantes");
-let contReceptor1   = document.getElementById("contador_receptor1");
-let contReceptor2   = document.getElementById("contador_receptor2");
-let contReceptor3   = document.getElementById("contador_receptor3");
-let contReceptor4   = document.getElementById("contador_receptor4");
-let contMovimientos = document.getElementById("contador_movimientos");
-
-// Tiempo
-let contTiempo  = document.getElementById("contador_tiempo"); // span cuenta tiempo
-let segundos 	 = 0;    // cuenta de segundos
-let temporizador = null; // manejador del temporizador
+const PALO_COLOR = {
+  viu: "orange",
+  cua: "orange",
+  hex: "grey",
+  cir: "grey",
+}; //TODO: Hacer readonly?
 
 /***** FIN DECLARACIÓN DE VARIABLES GLOBALES *****/
 
- 
-// Rutina asociada a boton reset
-/*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/
+function vaciarMazos() {
+  for (const mazo of document.getElementsByClassName("mazo"))
+    mazo.innerHTML = "";
+}
 
+function calcularContadorDeMazos(mazos) {
+  if (!mazos) mazos = document.getElementsByClassName("mazo");
+  for (const mazo of mazos) {
+    //Busco el contador asociado a el mazo y le pongo el largo del mazo
+    setContador(
+      mazo.parentElement.querySelector(".contador"),
+      mazo.children.length
+    );
+  }
+}
 
-// El juego arranca ya al cargar la página: no se espera a reiniciar
-/*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/
-
-// Desarrollo del comienzo de juego
 function comenzarJuego() {
-	console.log("Comenzando juego...");
-	/* Crear baraja, es decir crear el mazoInicial. Este será un array cuyos 
-	elementos serán elementos HTML <img>, siendo cada uno de ellos una carta.
-	Sugerencia: en dos bucles for, bárranse los "palos" y los "numeros", formando
-	oportunamente el nombre del fichero png que contiene a la carta (recuérdese poner
-	el path correcto en la URL asociada al atributo src de <img>). Una vez creado
-	el elemento img, inclúyase como elemento del array mazoInicial. 
-	*/
+  console.info("Comenzando juego...");
+  vaciarMazos(); //Vaciamos todo los elementos html que son un mazo
+  const mazo = generarMazo(); //Generamos el mazo
+  barajar(mazo); //Desordenamos el mazo
+  cargarMazoInicial(mazo); //Cargamos todos los elementos img en el dom
+  calcularContadorDeMazos(); //Calculamos el contador para TODOS los mazos.
+  setContador(contMovimientos, 0); //Contador de movimientos queda en cero.
+  arrancarTiempo(); //Arranca el conteo de tiempo
+}
 
-	/*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/	
-    
-	
-	// Barajar y dejar mazoInicial en tapete inicial
-	/*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/
+function generarMazo() {
+  const mazo = [];
+  for (let palo of Object.keys(PALO_COLOR)) {
+    for (let numero = 10; numero <= 12; numero++) {
+      let img = document.createElement("img");
+      img.src = `imagenes/baraja/${numero}-${palo}.png`;
+      img.alt = `${numero} de ${palo}`;
+      img.classList.add("carta");
+      img.id = `carta-${numero}-${palo}`; //Por si lo necesitamos.
+      img.draggable = false; //Solo la última será draggable.
+      img.dataset.numero = numero; //Guardamos el número en un data-attribute.
+      img.dataset.palo = palo; //Guardamos el palo en un data-attribute.
+      img.dataset.color = PALO_COLOR[palo];
+      img.addEventListener("dragstart", iniciaDrag); // Por ahora es suficiente solo con dragstart en la imagen.
+      mazo.push(img);
+    }
+  }
+  return mazo;
+}
 
-	// Puesta a cero de contadores de mazos
-	/*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/
-	
-	// Arrancar el conteo de tiempo
-	/*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/
+function arrancarTiempo() {
+  let contTiempo = document.getElementById("contador_tiempo"); // span cuenta tiempo
+  let segundos = 0; // cuenta de segundos
+  if (temporizador) clearInterval(temporizador);
+  let hms = function () {
+    let seg = Math.trunc(segundos % 60);
+    let min = Math.trunc((segundos % 3600) / 60);
+    let hor = Math.trunc((segundos % 86400) / 3600);
+    let tiempo =
+      (hor < 10 ? "0" + hor : "" + hor) +
+      ":" +
+      (min < 10 ? "0" + min : "" + min) +
+      ":" +
+      (seg < 10 ? "0" + seg : "" + seg);
+    setContador(contTiempo, tiempo);
+    segundos++;
+  };
+  segundos = 0;
+  hms(); // Primera visualización 00:00:00
+  temporizador = setInterval(hms, 1000);
+}
 
-} // comenzarJuego
-
-
-/**
-	Se debe encargar de arrancar el temporizador: cada 1000 ms se
-	debe ejecutar una función que a partir de la cuenta autoincrementada
-	de los segundos (segundos totales) visualice el tiempo oportunamente con el 
-	format hh:mm:ss en el contador adecuado.
-
-	Para descomponer los segundos en horas, minutos y segundos pueden emplearse
-	las siguientes igualdades:
-
-	segundos = truncar (   segundos_totales % (60)                 )
-	minutos  = truncar ( ( segundos_totales % (60*60) )     / 60   )
-	horas    = truncar ( ( segundos_totales % (60*60*24)) ) / 3600 )
-
-	donde % denota la operación módulo (resto de la división entre los operadores)
-
-	Así, por ejemplo, si la cuenta de segundos totales es de 134 s, entonces será:
-	   00:02:14
-
-	Como existe la posibilidad de "resetear" el juego en cualquier momento, hay que 
-	evitar que exista más de un temporizador simultáneo, por lo que debería guardarse
-	el resultado de la llamada a setInterval en alguna variable para llamar oportunamente
-	a clearInterval en su caso.   
-*/
-
-function arrancarTiempo(){
-	/*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/
-	if (temporizador) clearInterval(temporizador);
-    let hms = function (){
-			let seg = Math.trunc( segundos % 60 );
-			let min = Math.trunc( (segundos % 3600) / 60 );
-			let hor = Math.trunc( (segundos % 86400) / 3600 );
-			let tiempo = ( (hor<10)? "0"+hor : ""+hor ) 
-						+ ":" + ( (min<10)? "0"+min : ""+min )  
-						+ ":" + ( (seg<10)? "0"+seg : ""+seg );
-			setContador(contTiempo, tiempo);
-            segundos++;
-		}
-	segundos = 0;
-    hms(); // Primera visualización 00:00:00
-	temporizador = setInterval(hms, 1000);
-    	
-} // arrancarTiempo
-
-
-/**
-	Si mazo es un array de elementos <img>, en esta rutina debe ser
-	reordenado aleatoriamente. Al ser un array un objeto, se pasa
-	por referencia, de modo que si se altera el orden de dicho array
-	dentro de la rutina, esto aparecerá reflejado fuera de la misma.
-*/
 function barajar(mazo) {
-	/*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/	
+  if (!mazo || mazo.length === 0)
+    throw new Error("El mazo está vacío. No se puede barajar.");
+  //FIXME: Optimizar el desordenamiento.
+  mazo.sort(() => Math.random() - Math.random());
+  mazo.sort(() => Math.random() - Math.random());
+  mazo.sort(() => Math.random() - Math.random());
+  mazo[mazo.length - 1].draggable = true; //a la de más "arriba" le ponemos draggable.
 } // barajar
 
+function cargarMazoInicial(mazo) {
+  for (let i = mazo.length - 1; i >= 0; i--) {
+    mazoInicial.prepend(mazo[i]);
+  }
+}
 
-
-/**
- 	En el elemento HTML que representa el tapete inicial (variable tapeteInicial)
-	se deben añadir como hijos todos los elementos <img> del array mazo.
-	Antes de añadirlos, se deberían fijar propiedades como la anchura, la posición,
-	coordenadas top y left, algun atributo de tipo data-...
-	Al final se debe ajustar el contador de cartas a la cantidad oportuna
-*/
-function cargarTapeteInicial(mazo) {
-	/*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/	
-} // cargarTapeteInicial
-
-
-/**
- 	Esta función debe incrementar el número correspondiente al contenido textual
-   	del elemento que actúa de contador
-*/
-function incContador(contador){
-	/*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/	
-} // incContador
-
-/**
-	Idem que anterior, pero decrementando 
-*/
-function decContador(contador){
-	/*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! ***/	
-} // decContador
-
-/**
-	Similar a las anteriores, pero ajustando la cuenta al
-	valor especificado
-*/
 function setContador(contador, valor) {
-	/*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/
-} // setContador
+  if (!contador) throw new Error("No especificaste el contador.");
+  contador.textContent = valor;
+}
+
+function permitirDrop(event) {
+  event.preventDefault();
+}
+
+function iniciaDrag(event) {
+  const carta = event.target; //Lo unico que se puede arrastrar son las cartas.
+  event.dataTransfer.setData("idCarta", carta.id); //Guardamos el id de la carta que se está arrastrando.
+  event.dataTransfer.setData("idMazoOrigen", carta.parentElement.id); //También guardamos el mazo al que pertenece la carta. mazo es el padre de carta.
+}
+
+function cartaSoltada(event) {
+  event.preventDefault();
+  //Obtenemos los datos de la imágen arrastrada.
+  const idCarta = event.dataTransfer.getData("idCarta");
+  const idMazoOrigen = event.dataTransfer.getData("idMazoOrigen");
+  if (!idCarta || !idMazoOrigen) {
+    return console.error("No se ha obtenido id de carta o idMazoOrigen.");
+  }
+
+  const mazoDestino = event.target.closest(".mazo"); //Obtenemos el mazo destino
+  const mazoOrigen = document.getElementById(idMazoOrigen); //Obtenemos el mazo origen
+  if (mazoDestino === mazoOrigen) return; //Para que no cuente ni haga nada.
+  //Obtenemos la carta arrastrada
+  const cartaArrastrada = document.getElementById(idCarta);
+  if (!cartaArrastrada) {
+    console.error("No se ha obtenido elemento carta.");
+    return;
+  }
+  if (!mazoAceptaCarta(mazoDestino, cartaArrastrada)) return;
+
+  //En este punto ya sabemos que es válido mover la carta desde origen a destino.
+  if (mazoDestino.children.length !== 0)
+    mazoDestino.lastElementChild.draggable = false; //Hago no draggable la última carta del mazo destino
+  mazoDestino.appendChild(cartaArrastrada); //Añadimos la carta al mazoDestino. Automaticamente se borra del origen.
+  if (mazoOrigen.childElementCount !== 0)
+    mazoOrigen.lastElementChild.draggable = true; //Hago draggable la última carta del mazo origen
+
+  //Si el mazoInicial quedó vacío y sobrantes tiene elementos.
+  if (mazoInicial.childElementCount === 0) {
+    if (mazoSobrantes.childElementCount === 0)
+      return alert("Juego terminado. Ganaste.");
+    const mazo = [...mazoSobrantes.children]; //barajar acepta un array normal.
+    mazoSobrantes.innerHTML = "";
+    barajar(mazo);
+    cargarMazoInicial(mazo);
+    calcularContadorDeMazos([mazoSobrantes]);
+  }
+  calcularContadorDeMazos([mazoDestino, mazoOrigen]); //Actualizamos contadores, origen, destino y movimientos. Se opta por recalcular siempre el largo de los mazos involucrados. Ya que nos parece más seguro.
+  setContador(contMovimientos, parseInt(contMovimientos.textContent || 0) + 1); //Incrementamos el contador de movimientos
+  console.log("Sobrantes: ", mazoSobrantes.childElementCount);
+}
+
+function verificarMazoInicialVacio() {}
+
+function mazoAceptaCarta(mazoDiv, cartaImg) {
+  //Precondiciones
+  if (!mazoDiv?.id) throw new Error("No hay mazoDestino.");
+  if (!cartaImg?.id) throw new Error("No hay carta moviéndose.");
+  const { numero, palo, color } = cartaImg?.dataset || {};
+  if (!numero || !palo || !color)
+    throw new Error("Falta número, palo y/o color de la carta.");
+  if (mazoDiv.id === "mazo-inicial")
+    throw new Error("Mazo inicial no permite recibir cartas.");
+
+  if (mazoDiv.id === "mazo-sobrantes") return true; //Acepta cualquier carta.
+  if (!mazoDiv.id.includes("receptor"))
+    throw new Error("No se reconoce el mazo.");
+
+  //En este punto tiene que ser un mazo receptor. Verificar si el mazo receptor acepta la carta.
+  if (mazoDiv.children.length === 0) return numero === "12"; //Si el mazo es vacío solo acepta un 12.
+
+  const ultimaCarta = mazoDiv.lastElementChild;
+  const valActual = parseInt(ultimaCarta.dataset.numero);
+  const valNuevo = parseInt(numero);
+
+  //Si el mazo no es vacío, acepta una carta de distinto color y un número menor unicamente.
+  return ultimaCarta.dataset.color !== color && valActual - 1 === valNuevo;
+}
