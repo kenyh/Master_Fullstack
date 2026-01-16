0de22b6817104223304665f314e73fd1bdfb7bf8
alexmolina11 <alex.molina1194@gmail.com>
2026-01-16 16:24:39 -0600

    Actualización de archivos Actividad 1 - Bloques

--- backend/actividad1/web/app/views/index.php
+++ backend/actividad1/web/app/views/index.php
@@ -1,49 +1,56 @@
 <?php
 require_once __DIR__ . '/top.php';
 ?>
-<div class="row border">
-  <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-2">
-    <div class="card border-primary border-3 shadow">
-      <div class="card-body">
+<div class="row justify-content-center">
+  <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-2">
+    <div class="card h-100 border-primary border-3 shadow">
+      <div class="card-body d-flex flex-column">
         <h5 class="card-title">Plataformas</h5>
-        <p class="card-text">Una exaustiva lista de plataformas de streaming disponibles actualmente para ver todas tus series favoritas.</p>
-        <a href="/platforms/" class="btn btn-primary">Ir al listado</a>
+        <p class="card-text">
+          Una exhaustiva lista de plataformas de streaming disponibles actualmente
+          para ver todas tus series favoritas.
+        </p>
+        <a href="/platforms/" class="btn btn-primary mt-auto">Ir al listado</a>
       </div>
     </div>
   </div>
-  <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-2">
-    <div class="card border-primary border-3 shadow">
-      <div class="card-body">
+
+  <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-2">
+    <div class="card h-100 border-primary border-3 shadow">
+      <div class="card-body d-flex flex-column">
         <h5 class="card-title">Idiomas</h5>
         <p class="card-text">Lista de idiomas disponibles en las series.</p>
-        <a href="/languages/" class="btn btn-primary">Ir al listado</a>
+        <a href="/languages/" class="btn btn-primary mt-auto">Ir al listado</a>
       </div>
     </div>
   </div>
-  <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-2">
-    <div class="card border-primary border-3 shadow">
-      <div class="card-body">
+
+  <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-2">
+    <div class="card h-100 border-primary border-3 shadow">
+      <div class="card-body d-flex flex-column">
         <h5 class="card-title">Directores</h5>
         <p class="card-text">Lista de directores.</p>
-        <a href="/directors/" class="btn btn-primary">Ir al listado</a>
+        <a href="/directors/" class="btn btn-primary mt-auto">Ir al listado</a>
       </div>
     </div>
   </div>
-  <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-2">
-    <div class="card border-primary border-3 shadow">
-      <div class="card-body">
+
+  <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-2">
+    <div class="card h-100 border-primary border-3 shadow">
+      <div class="card-body d-flex flex-column">
         <h5 class="card-title">Actores y actrices</h5>
         <p class="card-text">Lista de actores y actrices.</p>
-        <a href="/actors/" class="btn btn-primary">Ir al listado</a>
+        <a href="/actors/" class="btn btn-primary mt-auto">Ir al listado</a>
       </div>
     </div>
   </div>
-  <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-2">
-    <div class="card border-primary border-3 shadow">
-      <div class="card-body">
+
+  <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-2">
+    <div class="card h-100 border-primary border-3 shadow">
+      <div class="card-body d-flex flex-column">
         <h5 class="card-title">Series</h5>
         <p class="card-text">Lista de series disponibles.</p>
-        <a href="/series/" class="btn btn-primary">Ir al listado</a>
+        <a href="/series/" class="btn btn-primary mt-auto">Ir al listado</a>
       </div>
     </div>
   </div>
