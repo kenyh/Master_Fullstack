bcfff91601c77a53e4d7afea03939a4404f2cd0b
Jorge Melnik <jmelnik19@gmail.com>
2026-02-07 11:33:25 -0300

    backend y entorno con docker creados

---
+++ zonavet/backend/routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+//Arranca en /api
+
+// Route::get('/', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('/', function () {
+    return response()->json(['root' => true]);
+});
