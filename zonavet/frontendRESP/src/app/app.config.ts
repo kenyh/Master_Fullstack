218d32cf36ef67c2dc8d45c178d1a7471eaa3b29
Jorge Melnik <jmelnik19@gmail.com>
2026-02-07 21:48:09 -0300

    a

--- zonavet/frontendRESP/src/app/app.config.ts
+++ zonavet/frontendRESP/src/app/app.config.ts
@@ -1,8 +1,7 @@
 // src/app/app.config.ts
 
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
